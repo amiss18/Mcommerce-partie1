900f86295291380014a28d87c603a84eb86b28a2
Chardan Missamou <chardanmiss@yahoo.fr>
2019-09-16 15:41:25 +0200

    P3-4: Validation du prix de vente et test des méthodes avec Postman

--- src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
+++ src/main/java/com/ecommerce/microcommerce/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.ecommerce.microcommerce.web.controller;
 
 import com.ecommerce.microcommerce.dao.ProductDao;
 import com.ecommerce.microcommerce.model.Product;
+import com.ecommerce.microcommerce.web.exceptions.ProduitGratuitException;
 import com.ecommerce.microcommerce.web.exceptions.ProduitIntrouvableException;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
@@ -68,13 +69,17 @@ public class ProductController {
 
     //ajouter un produit
     @PostMapping(value = "/Produits")
-
     public ResponseEntity<Void> ajouterProduit(@Valid @RequestBody Product product) {
 
         Product productAdded =  productDao.save(product);
 
         if (productAdded == null)
             return ResponseEntity.noContent().build();
+
+        if( product.getPrix() ==0 ) {
+            throw new ProduitGratuitException( String.format("Le prix de vente du produit d'ID %d ne peut être égal à 0", product.getId()));
+        }
+
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
